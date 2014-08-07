e27693dce1844cc47a393c624d56a04ea9054904
yutaro <y.tanaka.kcbbf@gmail.com>
2014-08-08 01:35:16 +0900

    Output using libraries.

--- hello.go
+++ hello.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Printf("hello, world\n")
+	
+	fmt.Println("The time is", time.Now());
+	
+	fmt.Println("And if you try to open a file:")
+	fmt.Println(os.Open("filename"))
+
+	fmt.Println("Or access the network:")
+	fmt.Println(net.Dial("tcp", "google.com"))
 }
