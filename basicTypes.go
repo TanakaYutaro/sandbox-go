17c6922973014c460ca728b6aafc34d95a1c2a25
yutaro <y.tanaka.kcbbf@gmail.com>
2014-08-14 02:16:18 +0900

    Numeric Constants.

--- basicTypes.go
+++ basicTypes.go
@@ -13,6 +13,18 @@ var (
     z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
+const (
+    Big = 1 << 100
+    Small = Big >> 99
+)
+
+func needInt(x int) int {
+    return x * 10 + 1
+}
+func needFloat(x float64) float64 {
+    return x * 0.1
+}
+
 func main() {
     const f = "%T(%v)\n"
     fmt.Printf(f, ToBe, ToBe)
@@ -25,4 +37,8 @@ func main() {
 
     const Truth = true
     fmt.Println("Go rules?", Truth)
+
+    fmt.Println(needInt(Small))
+    fmt.Println(needFloat(Small))
+    fmt.Println(needFloat(Big))
 }
